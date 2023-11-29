c187df339741484633aa6c55476f945e007019a9
Goes <ggoes.cm@gmail.com>
2023-11-29 11:23:53 -0300

    commit 1

---
+++ att.java
@@ -0,0 +1,10 @@
+// Exemplo de ClasseBase.java
+public class ClasseBase {
+    public void metodo1() {
+        System.out.println("Método 1 executado.");
+    }
+
+    public int metodo2(int a, int b) {
+        return a + b;
+    }
+}
